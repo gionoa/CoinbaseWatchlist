d1e425733a580b9ca1c7b0433e20849a37016ed7
gionoa <gnoa001@fiu.edu>
2019-04-11 17:39:33 -0400

    Initial Commit

---
+++ CoinbaseWatchlist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoinbaseWatchlist
+//
+//  Created by gnoa001 on 4/11/19.
+//  Copyright © 2019 Giovanni Noa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
