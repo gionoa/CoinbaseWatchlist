9dd34551bbe3fe9d1a5f337f50c50d0750271ed4
gionoa <gnoa001@fiu.edu>
2019-04-21 20:11:40 -0400

    Refactor Coin Cell to work properly with DynamicType

--- CoinbaseWatchlist/Views/CoinTableViewCell.swift
+++ CoinbaseWatchlist/Views/CoinTableViewCell.swift
@@ -28,6 +28,8 @@ class CoinTableViewCell: UITableViewCell {
         label.font = UIFont.preferredFont(forTextStyle: .headline)
         label.setContentHuggingPriority(.defaultLow, for: .vertical)
         label.setContentCompressionResistancePriority(.required, for: .vertical)
+        label.setContentCompressionResistancePriority(.defaultHigh, for: .horizontal)
+
         return label
     }()
     
@@ -35,9 +37,9 @@ class CoinTableViewCell: UITableViewCell {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.adjustsFontForContentSizeCategory = true
-        label.numberOfLines = 0 // change back to 0 if needed
+        label.numberOfLines = 1 // change back to 0 if needed
         label.font = UIFont.preferredFont(forTextStyle: .footnote)
-     //   label.setContentHuggingPriority(.defaultHigh, for: .vertical)
+       // label.setContentHuggingPriority(.defaultHigh, for: .vertical)
         label.textColor = .darkGray
         return label
     }()
@@ -48,9 +50,10 @@ class CoinTableViewCell: UITableViewCell {
         label.adjustsFontForContentSizeCategory = true
         label.adjustsFontSizeToFitWidth = true
         label.textAlignment = .right
-        label.numberOfLines = 0
-        label.font = UIFont.preferredFont(forTextStyle: .headline)
-        label.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+      //  label.numberOfLines = 2
+        label.font = UIFont.preferredFont(forTextStyle: .caption2)
+        label.setContentCompressionResistancePriority(.required, for: .horizontal)
+       // label.setContentHuggingPriority(.defaultHigh, for: .horizontal)
         return label
     }()
     
@@ -65,7 +68,6 @@ class CoinTableViewCell: UITableViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
-
     // MARK: - Setup
     private func setupCell() {
         contentView.addSubview(coinImageView)
@@ -82,25 +84,25 @@ class CoinTableViewCell: UITableViewCell {
             coinImageView.heightAnchor.constraint(equalToConstant: 52),//: coinTitleLabel.heightAnchor),
             coinImageView.widthAnchor.constraint(equalToConstant: 52), //coinImageView.heightAnchor),
             coinImageView.leadingAnchor.constraint(equalTo: margins.leadingAnchor),
-     //       coinImageView.topAnchor.constraint(greaterThanOrEqualTo: margins.topAnchor),
-        //    coinImageView.bottomAnchor.constraint(lessThanOrEqualTo: margins.bottomAnchor),
+            coinImageView.topAnchor.constraint(greaterThanOrEqualTo: margins.topAnchor),
+            coinImageView.bottomAnchor.constraint(lessThanOrEqualTo: margins.bottomAnchor),
             coinImageView.centerYAnchor.constraint(equalTo: margins.centerYAnchor),
         
-            coinTitleLabel.topAnchor.constraint(equalTo: margins.topAnchor, constant: inset),
-         //   coinTitleLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor, constant: -inset),
+            coinTitleLabel.topAnchor.constraint(equalTo: margins.topAnchor, constant: inset / 2),
             coinTitleLabel.leadingAnchor.constraint(equalTo: coinImageView.trailingAnchor, constant: inset),
-            coinTitleLabel.trailingAnchor.constraint(equalTo: priceLabel.leadingAnchor, constant: -inset),
+            coinTitleLabel.trailingAnchor.constraint(lessThanOrEqualTo: priceLabel.leadingAnchor, constant: -inset),
             coinTitleLabel.bottomAnchor.constraint(lessThanOrEqualTo: tickerSymbolLabel.topAnchor),
             
             tickerSymbolLabel.topAnchor.constraint(equalTo: coinTitleLabel.bottomAnchor, constant: (inset / 2)),
             tickerSymbolLabel.leadingAnchor.constraint(equalTo: coinImageView.trailingAnchor, constant: inset),
-            tickerSymbolLabel.trailingAnchor.constraint(equalTo: priceLabel.leadingAnchor, constant: -inset),
-            tickerSymbolLabel.bottomAnchor.constraint(lessThanOrEqualTo: margins.bottomAnchor, constant: -inset),
+            tickerSymbolLabel.trailingAnchor.constraint(lessThanOrEqualTo: priceLabel.leadingAnchor, constant: -inset),
+            tickerSymbolLabel.bottomAnchor.constraint(lessThanOrEqualTo: margins.bottomAnchor),
             
             priceLabel.trailingAnchor.constraint(equalTo: margins.trailingAnchor, constant: inset),
-            priceLabel.leadingAnchor.constraint(equalTo: coinTitleLabel.trailingAnchor, constant: inset),
+            priceLabel.leadingAnchor.constraint(equalTo: coinTitleLabel.trailingAnchor),
             priceLabel.centerYAnchor.constraint(equalTo: margins.centerYAnchor),
-            priceLabel.bottomAnchor.constraint(lessThanOrEqualTo: margins.bottomAnchor)
+          //  priceLabel.topAnchor.constraint(equalTo: tickerSymbolLabel.topAnchor),
+            priceLabel.bottomAnchor.constraint(equalTo: margins.bottomAnchor, constant: -inset)
             ])
     }
     
